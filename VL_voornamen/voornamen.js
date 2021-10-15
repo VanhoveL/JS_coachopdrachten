6cc76bb171d44c68b7ace32635484c2720c73bf0
Lotje Vanhove <Lotje.Vanhove@vdabcampus.be>
2021-10-15 13:45:32 +0200

    functies toegevoegd, met tussenversies in commentaar (array.foreach probleem)

--- VL_voornamen/voornamen.js
+++ VL_voornamen/voornamen.js
@@ -9,35 +9,68 @@ submit.addEventListener('click', function() {
     const input = document.getElementById('vnaam');
     input.value="";
     input.focus();
+    let gevondenLiItem = zoekListItemMetNaam(naamInput); //lielement of null
 
-    // al bestaande namen ophalen en vergelijken
-    const liElementen = document.getElementsByTagName('li');
-    var found = false;
-    Array.from(liElementen).forEach(function(liElement) {
-        let naamLijst = liElement.dataset.naam;
-        if (naamLijst == naamInput) {
-            liElement.dataset.aantal = Number(liElement.dataset.aantal) + 1;
-            liElement.textContent = `${liElement.dataset.naam}: ${liElement.dataset.aantal}`
-            found = true;
-        }
-    }); 
+    if (gevondenLiItem !== null) { 
+        verhoogAantal(gevondenLiItem, 1);
+    } else { 
+        maakListItemMetNaam(naamInput);
 
-
-    if (!found) {
-
-         //aanmaken element
-         const liItem = document.createElement('li');
-         liItem.setAttribute("data-naam", naamInput);
-         liItem.setAttribute("data-aantal", "1");
-
-         //inhoud toevoegen
-         liItem.textContent = `${liItem.dataset.naam}: ${liItem.dataset.aantal}`
-
-         //aan DOM toevoegen
-         const list = document.querySelector('#namenlijst ul');
-         list.appendChild(liItem);
     }
+    /*
+    IF (zoeklistmetnaam(naamInput) iets teruggeeft omdat naam al bestaat -> liElement) {
+        verhoogaantal(liElement, aantal)
+    } ELSE //naam bestaat nog niet in lijst {
+        maaklistitemmetnaam(naamInput)
+    }
+    */
+
 });
 
+// FUNCTIES
+function zoekListItemMetNaam(naamInput) {
+    const liElementen = document.getElementsByTagName('li');
+    let gevondenListItem = null;
+    Array.from(liElementen).forEach(function(liElement) {
+        let naamLijst = liElement.dataset.naam;
+        if (naamLijst === naamInput) {
+            gevondenListItem = liElement;
+            return;
+        }
+    });
 
 
+
+    /*
+    for (const li of liElementen) {
+        if (naamInput === li.dataset.naam) {
+            return li;
+        }
+    }
+    */
+    return gevondenListItem;
+}
+
+
+function maakListItemMetNaam(naamInput) {
+    //aanmaken element
+    const liItem = document.createElement('li');
+    /*
+    liItem.setAttribute("data-naam", naamInput);
+    liItem.setAttribute("data-aantal", "1");
+    */
+    liItem.dataset.naam = naamInput;
+    liItem.dataset.aantal = 1;
+    //inhoud toevoegen
+    liItem.textContent = `${liItem.dataset.naam}: ${liItem.dataset.aantal}`
+    //aan DOM toevoegen
+    const list = document.querySelector('#namenlijst ul');
+    list.appendChild(liItem);
+};
+
+
+function verhoogAantal(liElement, aantal) {
+    let vorigAantal = Number(liElement.dataset.aantal);
+    liElement.dataset.aantal++;
+    liElement.textContent = `${liElement.dataset.naam}: ${vorigAantal + aantal}`;
+}
