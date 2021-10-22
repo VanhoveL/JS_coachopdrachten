3a6aeaade57f0b5c7dd94bd8892b0eeb60b6e392
Lotje Vanhove <Lotje.Vanhove@vdabcampus.be>
2021-10-22 15:45:02 +0200

    coachopdracht 'geslachten' keuzemenu toegevoegd + werkt al. Maar code nog mooier maken

--- VL_geslachten/geslachten.js
+++ VL_geslachten/geslachten.js
@@ -6,6 +6,9 @@ async function leesBestand() {
     if (response.ok) {
         const personen = await response.json(); 
         displayPersonen(personen);
+        displayVrouwen(personen);
+        displayMannen(personen);
+        displayAllen(personen);
 
     } else {
         document.getElementById("nietGevonden").hidden = false;
@@ -13,27 +16,72 @@ async function leesBestand() {
 }
 
 
-function displayPersonen(personen) {
-    for (const persoon of personen) {
-        const tbody = document.querySelector("tbody");
-        const tr = tbody.insertRow();
-        const vnaamTd = tr.insertCell();
-        vnaamTd.innerText = persoon.voornaam;
-        const fnaamTd = tr.insertCell();
-        fnaamTd.innerText = persoon.familienaam;
 
-        //geslacht icoon
-        const geslachtTd = tr.insertCell(); 
-        let icoonNaam = `${persoon.geslacht}.png`;
-        geslachtTd.setAttribute('id', `${persoon.voornaam}${persoon.familienaam}`);
-        add_img(icoonNaam, `${persoon.voornaam}${persoon.familienaam}`);
-        
-        //foto afbeelding 
-        const fotoTd = tr.insertCell();
-        fotoTd.setAttribute('id', persoon.foto)
-        add_img(persoon.foto, persoon.foto);  
+function displayVrouwen(personen) {
+    document.getElementById('vrouwen').onclick = function() {
+        let vrouwen = [];
+        for (const persoon of personen) {
+            if(persoon.geslacht == 'vrouw'){
+                vrouwen.push(persoon);
+                
+            }
+        }
+        while (document.getElementById('tbody').lastChild !== null) {
+            document.getElementById('tbody').lastChild.remove();
+        }
+        displayPersonen(vrouwen);
+    }
+}
+
+function displayMannen(personen) {
+    document.getElementById('mannen').onclick = function() {
+        let mannen = [];
+        for (const persoon of personen) {
+            if(persoon.geslacht == 'man'){
+                mannen.push(persoon);
+            }
+        }
+        while (document.getElementById('tbody').lastChild !== null) {
+            document.getElementById('tbody').lastChild.remove();
+        }
+           
+        displayPersonen(mannen);
+    }
+}
+
+
+function displayAllen(personen) {
+    document.getElementById('allen').onclick = function () {
+        while (document.getElementById('tbody').lastChild !== null) {
+            document.getElementById('tbody').lastChild.remove();
+        }
+
+        displayPersonen(personen);
     }
 
+}
+
+
+function displayPersonen(personen) {
+        for (const persoon of personen) {
+            const tbody = document.querySelector("tbody");
+            const tr = tbody.insertRow();
+            const vnaamTd = tr.insertCell();
+            vnaamTd.innerText = persoon.voornaam;
+            const fnaamTd = tr.insertCell();
+            fnaamTd.innerText = persoon.familienaam;
+
+            //geslacht icoon
+            const geslachtTd = tr.insertCell(); 
+            let icoonNaam = `${persoon.geslacht}.png`;
+            geslachtTd.setAttribute('id', `${persoon.voornaam}${persoon.familienaam}`);
+            add_img(icoonNaam, `${persoon.voornaam}${persoon.familienaam}`);
+            
+            //foto afbeelding 
+            const fotoTd = tr.insertCell();
+            fotoTd.setAttribute('id', persoon.foto)
+            add_img(persoon.foto, persoon.foto);  
+        }
 }
 
 
