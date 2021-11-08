06d5aec1f0a7783aae1a4f4662e6d675fac2ce52
Lotje Vanhove <lotje.vanhove@hotmail.com>
2021-11-08 13:32:10 +0100

    efficienter maken -> geslacht filter nog doen werken

--- VL_geslachten/geslachten.js
+++ VL_geslachten/geslachten.js
@@ -5,10 +5,10 @@ async function leesBestand() {
     const response = await fetch("geslachten.json")
     if (response.ok) {
         const personen = await response.json(); 
-        displayPersonen(personen);
-        displayVrouwen(personen);
-        displayMannen(personen);
-        displayAllen(personen);
+        maakTabel(personen);
+        //displayVrouwen(personen);
+        //displayMannen(personen);
+        //displayAllen(personen);
 
     } else {
         document.getElementById("nietGevonden").hidden = false;
@@ -16,8 +16,69 @@ async function leesBestand() {
 }
 
 
+function maakTabel(personen) {
+    for (const persoon of personen) {
+        const tbody = document.querySelector("tbody");
+        const tr = tbody.insertRow();
+        const vnaamTd = tr.insertCell();
+        vnaamTd.innerText = persoon.voornaam;
+        const fnaamTd = tr.insertCell();
+        fnaamTd.innerText = persoon.familienaam;
 
-function displayVrouwen(personen) {
+        //geslacht icoon
+        const geslachtTd = tr.insertCell(); 
+        let icoonNaam = `${persoon.geslacht}.png`;
+        geslachtTd.setAttribute('id', `${persoon.voornaam}${persoon.familienaam}`);
+        add_img(icoonNaam, `${persoon.voornaam}${persoon.familienaam}`);
+        
+        //foto afbeelding 
+        const fotoTd = tr.insertCell();
+        fotoTd.setAttribute('id', persoon.foto)
+        add_img(persoon.foto, persoon.foto);  
+
+    } return personen;
+}
+
+
+
+document.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => {
+        let sexe = a.dataset.geslacht;
+        console.log(sexe);
+        let table = document.querySelectorAll('tbody tr');
+        for (const tr of table) {
+            console.log(tr); //hoe moet ik de img naam hieruit krijgen?
+            let x = document.getElementsByTagName('img').getAttribute('src');
+            console.log(x);
+            // BETER GESLACHT OPNEMEN ALS CLASS...?
+            
+
+        }
+    });
+});
+
+//als geslacht niet overeenkomt (false is) -> hide
+/*
+if (! string.includes(sexe)) { 
+
+} 
+*/
+
+
+
+
+
+function add_img(fotoNaam, idCell) { 
+    let img = document.createElement('img'); 
+    img.src = `./images/${fotoNaam}`;
+    document.getElementById(`${idCell}`).appendChild(img);
+}
+
+
+
+
+
+//function displayVrouwen(personen) {
     document.getElementById('vrouwen').onclick = function() {
         let vrouwen = [];
         for (const persoon of personen) {
@@ -29,9 +90,9 @@ function displayVrouwen(personen) {
         while (document.getElementById('tbody').lastChild !== null) {
             document.getElementById('tbody').lastChild.remove();
         }
-        displayPersonen(vrouwen);
+        maakTabel(vrouwen);
     }
-}
+//}
 
 function displayMannen(personen) {
     document.getElementById('mannen').onclick = function() {
@@ -45,7 +106,7 @@ function displayMannen(personen) {
             document.getElementById('tbody').lastChild.remove();
         }
            
-        displayPersonen(mannen);
+        maakTabel(mannen);
     }
 }
 
@@ -56,39 +117,8 @@ function displayAllen(personen) {
             document.getElementById('tbody').lastChild.remove();
         }
 
-        displayPersonen(personen);
+        maakTabel(personen);
     }
 
 }
-
-
-function displayPersonen(personen) {
-        for (const persoon of personen) {
-            const tbody = document.querySelector("tbody");
-            const tr = tbody.insertRow();
-            const vnaamTd = tr.insertCell();
-            vnaamTd.innerText = persoon.voornaam;
-            const fnaamTd = tr.insertCell();
-            fnaamTd.innerText = persoon.familienaam;
-
-            //geslacht icoon
-            const geslachtTd = tr.insertCell(); 
-            let icoonNaam = `${persoon.geslacht}.png`;
-            geslachtTd.setAttribute('id', `${persoon.voornaam}${persoon.familienaam}`);
-            add_img(icoonNaam, `${persoon.voornaam}${persoon.familienaam}`);
-            
-            //foto afbeelding 
-            const fotoTd = tr.insertCell();
-            fotoTd.setAttribute('id', persoon.foto)
-            add_img(persoon.foto, persoon.foto);  
-        }
-}
-
-
-function add_img(fotoNaam, idCell) { 
-    let img = document.createElement('img'); 
-    img.src = `./images/${fotoNaam}`;
-    document.getElementById(`${idCell}`).appendChild(img);
-}
-
 
