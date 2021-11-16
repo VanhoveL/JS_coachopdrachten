03f931116354fa7746cb07dab0037ad9682760cc
Lotje Vanhove <lotje.vanhove@hotmail.com>
2021-11-16 09:53:57 +0100

    validatie, toevoegen rij en verwijderen rij aanwezig

--- VL_groentenwinkel/groentenwinkel.js
+++ VL_groentenwinkel/groentenwinkel.js
@@ -6,6 +6,7 @@ async function leesKeuzeOpties() {
     if (response.ok) {
         const groenten = await response.json(); 
         maakKeuzeMenu(groenten);
+        valideerInput();
     
     } else {
         document.getElementById("nietGevonden").hidden = false;
@@ -22,3 +23,70 @@ function maakKeuzeMenu(groenten) {
         
     }
 }
+
+function valideerInput(groenten){
+    document.getElementById("toevoegen").onclick = function () {
+        const verkeerdeElementen =
+            document.querySelectorAll("select:invalid,input:invalid");
+        for (const element of verkeerdeElementen) {
+            document.getElementById(`${element.id}Fout`).hidden = false;
+        }
+        const correcteElementen =
+            document.querySelectorAll("input:valid,select:valid");
+        for (const element of correcteElementen) {
+            document.getElementById(`${element.id}Fout`).hidden = true;
+        }
+        if (verkeerdeElementen.length == 0){
+            document.getElementById("toegevoegd").hidden = false;
+            let keuzeInput = document.getElementById("keuze").value;
+            let aantalInput = document.getElementById("aantal").value;
+            voegToeAanMandje(keuzeInput, aantalInput);
+        }
+    };
+
+}
+
+
+
+
+function voegToeAanMandje(keuzeInput, aantalInput) {
+    const tbody = document.querySelector("tbody");
+    const tr = tbody.insertRow();
+    //groente
+    const gekozenGroenteTd = tr.insertCell();
+    gekozenGroenteTd.innerText = keuzeInput.split(" ")[0]; 
+
+    //aantal
+    const gekozenAantalTd = tr.insertCell();
+    gekozenAantalTd.innerText = aantalInput;
+
+    //prijs
+    let prijs = keuzeInput.substring(
+        keuzeInput.indexOf("(") + 1,
+        keuzeInput.lastIndexOf("/")
+    );
+    const prijsTd = tr.insertCell();
+    prijsTd.innerText = prijs; 
+
+    //te betalen
+    const teBetalenTd = tr.insertCell();
+    teBetalenTd.innerText = (prijs*aantalInput).toFixed(2); 
+
+    //vuilbakje
+    const verwijderTd = tr.insertCell();
+    verwijderTd.setAttribute('id', 'verwijder');
+    verwijderTd.innerHTML = "<img id='vuilbak' src='./vuilbak.png' alt='verwijder'/>";
+    verwijderTd.addEventListener('click', () => {
+        tr.remove();
+        //updateTotaalprijs();
+    })
+
+}
+
+/*
+
+function updateTotaalprijs(){
+
+}
+
+*/
